246e81dc04d4a66d652236226db835dc431aea1a
Duver Melo <duvermelo2606@gmail.com>
2024-03-13 21:04:46 -0500

    Avance de proyectoi de teslo app del curso de flutter de fernando herrera

---
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/config.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'package:teslo_shop/features/products/infrastructure/infrastructure.dart';
+
+class ProductsDatasourceImpl extends ProductsDatasource {
+
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({
+    required this.accessToken
+  }) : dio = Dio(
+    BaseOptions(
+      baseUrl: Enviroment.apiUrl,
+      headers: {
+        'Authorization': 'Bearer $accessToken'
+      }
+    )
+  );
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    // TODO: implement createUpdateProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Product> getProductId(String id) async {
+    try {
+      final response = await dio.get('/products/$id');
+      final product = ProductMapper.jsonToEntity(response.data);
+
+      return product;
+    } on DioException catch(e) {
+      if (e.response!.statusCode == 404 ) throw ProductNotFound();
+      throw Exception();
+    } catch (e) {
+      throw Exception();
+    }
+  }
+
+  @override
+  Future<List<Product>> getProductsByPage({int limit = 10, offset = 0}) async {
+    
+    final response = await dio.get<List>('/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
+    for (final product in response.data ?? [] ) {
+      products.add(ProductMapper.jsonToEntity(product));
+    }
+
+    return products;
+  }
+
+  @override
+  Future<List<Product>> searchProductsByTerm(String term) {
+    // TODO: implement searchProductsByTerm
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/features/products/infrastructure/repositories/products_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsRepositoryImpl extends ProductsRepository {
+  
+  final ProductsDatasource datasource;
+
+  ProductsRepositoryImpl(this.datasource);
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    return datasource.createUpdateProduct(productLike);
+  }
+
+  @override
+  Future<Product> getProductId(String id) {
+    return datasource.getProductId(id);
+  }
+
+  @override
+  Future<List<Product>> getProductsByPage({int limit = 10, offset = 0}) {
+    return datasource.getProductsByPage(limit: limit, offset: offset);
+  }
+
+  @override
+  Future<List<Product>> searchProductsByTerm(String term) {
+    return datasource.searchProductsByTerm(term);
+  }
+
+}
